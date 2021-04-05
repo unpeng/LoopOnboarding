0449035db47c763a3f5d5b41511491b56bc7018a
Pete Schwamb <pete@schwamb.net>
2021-04-05 08:38:02 -0700

    Add exmample app

---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Pete Schwamb on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
